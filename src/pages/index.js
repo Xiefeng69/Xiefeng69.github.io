daf7c8f3a1324d526222603ddbd54fc945ed551a
Xiefeng69 <xiefeng0526@foxmail.com>
2021-11-27 23:07:12 +0800

    init: 代码结构调整

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Mymenu from '../components/mymenu'
+import Intro from '../components/intro'
+import Experience from '../components/experience'
 
 const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+  <div>
+    <Mymenu />
+    <Intro />
+    <Experience />
+  </div>
 )
 
 export default IndexPage
