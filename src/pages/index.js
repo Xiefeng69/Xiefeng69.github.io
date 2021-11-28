62a78cabba30ef1ff911df083aca8513e05b4dbe
Xiefeng69 <xiefeng0526@foxmail.com>
2021-11-28 21:15:47 +0800

    fix: AntDesign全局注入

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Right from '../partition/right'
 import Left from "../partition/left";
 import './index.css';
+import "antd/dist/antd.css";
 
 const IndexPage = () => (
   <div className="main">
