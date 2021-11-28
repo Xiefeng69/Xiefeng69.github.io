67ca1f4bd0e55d7d1bf79775983972eaf4202439
Xiefeng69 <xiefeng0526@foxmail.com>
2021-11-28 23:04:11 +0800

    feat: experient部分

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,12 +1,40 @@
-import * as React from 'react'
-import Subtitle from './subtitle'
+import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import Subtitle from './subtitle';
+import * as style from './experience.module.css';
 
 const Experience = () => {
     return (
         <div>
             <Subtitle title="Experience" />
+            <div className={style.container}>
+                <StaticImage className={style.logo} src="../images/nudt.png" alt="NUDT" />
+                <div className={style.infocontainer}>
+                    <span className={style.subinfo}>2021.09 - present</span>
+                    <br />
+                    <span className={style.maininfo}>National University of Defense Technology, <i>Master.</i></span>
+                </div>
+            </div>
+
+            <div className={style.container}>
+                <StaticImage className={style.logo} src="../images/xiaomi.png" alt="Xiaomi" />
+                <div className={style.infocontainer}>
+                    <span className={style.subinfo}>2020.09 - 2021.01</span>
+                    <br />
+                    <span className={style.maininfo}>Xiaomi Technology Co., Ltd., Mi Group-1<i>Software Development Intern.</i></span>
+                </div>
+            </div>
+
+            <div className={style.container} style={{marginBottom: '10px'}}>
+                <StaticImage className={style.logo} src="../images/dut.png" alt="DUT" />
+                <div className={style.infocontainer}>
+                    <span className={style.subinfo}>2017.09 - 2021.06</span>
+                    <br />
+                    <span className={style.maininfo}>Dalian University of Technology, <i>Bachelor.</i></span>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Experience
+export default Experience;
