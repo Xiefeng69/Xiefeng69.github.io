0eafe570be31a84b7f794aade8ce012a0c6e5875
Xiefeng69 <854438622@qq.com>
2021-11-30 15:40:28 +0800

    fix: 调整index.js布局

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import * as React from "react"
+import "antd/dist/antd.css";
 import Right from '../partition/right'
 import Left from "../partition/left";
+import IsDesktop from "../utils/isdesktop";
 import './index.css';
-import "antd/dist/antd.css";
 
 const IndexPage = () => (
   <div className="main">
     <Left />
+    <IsDesktop>
+      <div style={{width:'24px'}}></div>
+    </IsDesktop>
     <Right />
   </div>
 )
