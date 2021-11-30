b1232cb1528ecb36adbde9dbe0b59183292ec0d6
Xiefeng69 <854438622@qq.com>
2021-11-30 15:55:26 +0800

    feat: 添加footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,20 @@ import * as React from "react"
 import "antd/dist/antd.css";
 import Right from '../partition/right'
 import Left from "../partition/left";
+import Footer from '../partition/footer';
 import IsDesktop from "../utils/isdesktop";
 import './index.css';
 
 const IndexPage = () => (
-  <div className="main">
-    <Left />
-    <IsDesktop>
-      <div style={{width:'24px'}}></div>
-    </IsDesktop>
-    <Right />
+  <div>
+    <div className="main">
+      <Left />
+      <IsDesktop>
+        <div style={{width:'24px'}}></div>
+      </IsDesktop>
+      <Right />
+    </div>
+    <Footer />
   </div>
 )
 
