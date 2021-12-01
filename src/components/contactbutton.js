6966d48a5cd3e9fd9e4e33be5b6e1dae4df38b7b
Xiefeng69 <854438622@qq.com>
2021-12-01 15:24:47 +0800

    fix: modify the style of concat button toolbox

--- src/components/contactbutton.js
+++ src/components/contactbutton.js
@@ -1,23 +1,42 @@
 import React from "react";
+import { message } from "antd";
 import { StaticImage } from "gatsby-plugin-image";
+import IsMobile from '../utils/ismobile';
 import * as style from './contactbutton.module.css';
-import githublogo from '../images/github.png';
-import wechatlogo from '../images/wechat.png';
-import emaillogo from '../images/email.png';
 
-const ContactButton = (props) => {
+const ContactButton = () => {
+    const copyInfo = (type, info) => {
+        //document.execCommand
+        navigator.clipboard.writeText(info).then(()=>{
+            message.success({
+                content: `My ${type} has been copied`,
+                duration: 2,
+                style: {
+                    marginTop: '5vh'
+                }
+            })
+        }).catch(() => {
+            message.warning('Please click it again', 2)
+        })
+    }
     return (
         <div className={style.buttoncontainer}>
             <div>
                 <a href="https://github.com/Xiefeng69" target="_blank">
-                    <img src={githublogo} />
+                    <StaticImage src="../images/github.png" />
                 </a>
             </div>
-            <div>
-                <img src={wechatlogo} />
+            <IsMobile>
+                <div style={{width: '20px'}}></div> 
+            </IsMobile>
+            <div onClick={() => {copyInfo('Wechat', 'xf16943')}}>
+                <StaticImage src="../images/wechat.png" />
             </div>
-            <div>
-                <img src={emaillogo} />
+            <IsMobile>
+                <div style={{width: '20px'}}></div>
+            </IsMobile>
+            <div onClick={() => copyInfo('email', 'xiefeng0526@foxmail.com')}>
+                <StaticImage src="../images/email.png" />
             </div>
         </div>
     )
