831bb0fea77a2af9e41c23182cff0692fb55afc5
Xiefeng69 <854438622@qq.com>
2021-12-02 15:47:42 +0800

    fix: fix the anchor

--- src/components/anchor.js
+++ src/components/anchor.js
@@ -1,8 +1,23 @@
-import React from "react";
-import { Portal } from "react-portal";
+import React, { useState } from "react";
+import styled from "styled-components";
 import * as style from './anchor.module.css';
 
+const AnchorContainer = styled.div`
+    padding: 10px 10px 5px 10px;
+    width: 100%;
+    background: white;
+    box-shadow: 0 4px 13px -3px rgb(0 0 0 / 10%);
+    border-bottom: 1px solid #d2d2d2;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    position: fixed;
+    top: 0px;
+    z-index: 100;
+`
+
 const Anchor = () => {
+    const [panelVisible, setPanelVisible] = useState(false)
     const scrollTo = (idx) => {
         /*
         let scrollTarget = document.getElementsByClassName('anchorlink')[idx].offsetTop
@@ -15,14 +30,29 @@ const Anchor = () => {
             behavior: "smooth"
         })
     }
+    const switchVisible = () => {
+        setPanelVisible(!panelVisible)
+    }
     return (
-        <Portal>
-            <div className={style.container}>
-                <p onClick={() => {scrollTo(0)}}>Experiences</p>
+        <AnchorContainer>
+            <div className={style.anchorNav}>
+                <span onClick={() => {scrollTo(0)}}>Experiences</span>
                 <p onClick={() => {scrollTo(1)}}>Projects</p>
                 <p onClick={() => {scrollTo(2)}}>Awards</p>
+                <p onClick={switchVisible}>x</p>
             </div>
-        </Portal>
+            <div className={style.anchorPart}>
+                {
+                    panelVisible ?
+                    <div className={style.anchorPanel}>
+                        <span>Experiences</span>
+                        <span>Projects</span>
+                        <span>Awards</span>
+                    </div>
+                    : null
+                }
+            </div>
+        </AnchorContainer> 
     )
 }
 
