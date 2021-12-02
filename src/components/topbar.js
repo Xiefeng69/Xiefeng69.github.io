fa343b11bd288d9777785048eaa73168986df26f
Xiefeng69 <854438622@qq.com>
2021-12-02 21:22:11 +0800

    fix: 修复上线图片问题“

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-import ContactButton from './contactbutton';
 import Anchor from './anchor';
 import Introduction from './introduction';
 import * as style from './topbar.module.css';
