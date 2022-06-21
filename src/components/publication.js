7b255b0edd582414031becc38ff43634dae7d2e4
Xiefeng69 <854438622@qq.com>
2022-06-21 12:28:36 +0800

    fix: publication boldface

--- src/components/publication.js
+++ src/components/publication.js
@@ -9,14 +9,15 @@ const Publication = () => {
         <div>
             <Subtitle title="Publications"></Subtitle>
             <ul className={style.contrainer} style={{lineHeight:1.4}}>
-                <li>Feng Xie, Zhong Zhang, Liang Li, Bin Zhou and Yusong Tan.
+                <li>
+                    <span style={{fontWeight:'bold'}}>Feng Xie</span>, Zhong Zhang, Liang Li, Bin Zhou and Yusong Tan.
                     &nbsp;"EpiGNN: Exploring Spatial Transmission with Graph Neural Network for Regional Epidemic Forecasting".
                     &nbsp;<span style={{fontStyle:'italic'}}>The European Conference on Machine Learning and Principles and Practice of Knowledge Discovery in Databases</span>, 
                     &nbsp;<span style={{fontWeight:'bold'}}>ECML-PKDD </span>(CCF-B)
                     &nbsp;<a href="https://github.com/Xiefeng69/EpiGNN" target="_blank">[Code]</a>
                 </li>
                 <li>
-                    Feng Xie, Zhong Zhang, Xuechen Zhao, Bin Zhou and Yusong Tan.
+                <span style={{fontWeight:'bold'}}>Feng Xie</span>, Zhong Zhang, Xuechen Zhao, Bin Zhou and Yusong Tan.
                     &nbsp;"Inter- and Intra-Series Embeddings Fusion Network for Epidemiological Forecasting".
                     &nbsp;<span style={{fontStyle:'italic'}}>The 34th International Conference on Software Engineering & Knowledge Engineering</span>,
                     &nbsp;<span style={{fontWeight:'bold'}}>SEKE2022</span> (CCF-C)
